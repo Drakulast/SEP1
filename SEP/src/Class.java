77adef87ee58cb348bdfc2b70a97fc1978c6e705
Ionut Grosu <ionutflorentin.grosu@gmail.com>
2020-05-18 22:06:16 +0200

    added Class class

--- SEP/src/Class.java
+++ SEP/src/Class.java
@@ -3,5 +3,35 @@ public class Class
   private String name;
   private int maxCapacity;
 
-  public
+  public Class(String name, int maxCapacity)
+  {
+    this.name = name;
+    this.maxCapacity = maxCapacity;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public void setMaxCapacity(int maxCapacity)
+  {
+    this.maxCapacity = maxCapacity;
+  }
+
+  public String toString()
+  {
+    return "Class{" + "name='" + name + '\'' + ", maxCapacity=" + maxCapacity
+        + '}';
+  }
+
+  public boolean equals(Object  obj)
+  {
+    if (!(obj instanceof Class))
+    {
+      return false;
+    }
+    Class other = (Class) obj;
+    return name.equals(other.name) && maxCapacity == other.maxCapacity;
+  }
 }
